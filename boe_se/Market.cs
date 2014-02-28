4316aed8d0fb1a5f9a1a659bef064de4f815078e
Philip Wellnitz <philipwellnitz@gmx.de>
2014-02-28 13:55:53 +0100

    Yay!

--- boe_se/Market.cs
+++ boe_se/Market.cs
@@ -5,36 +5,48 @@ using Newtonsoft.Json;
 
 namespace boe_se
 {
-	namespace Engine{
-		public class Market
-		{			
-			/// <summary>
-			/// The items.
-			/// </summary>
+    namespace Engine
+    {
+        public class Market
+        {
+            /// <summary>
+            /// The items.
+            /// </summary>
             public List<GItem> Items;
             private WebClient WClient;
 
-			private static Lazy<Market> Instance = new Lazy<Market>(() => new Market());
-			public static Market GetInstance {
-				get {
-					return Instance.Value;
-				}
-			}
+            private static Lazy<Market> Instance = new Lazy<Market>(() => new Market());
+            public static Market GetInstance
+            {
+                get
+                {
+                    return Instance.Value;
+                }
+            }
 
-			private Market ()
-			{
+            class GAllItemsResponse
+            {
+                [JsonProperty("results")]
+                public List<GItem> Result;
+            }
+
+            private Market()
+            {
                 WClient = new WebClient();
-                string allItems = WClient.DownloadString("http://www.gw2spidy.com/api/v0.9/json/all-items/all");
-                Items=JsonConvert.DeserializeObject<Dictionary<string, List<GItem>>>(allItems)["result"];
-			}
-			
-			public GItem GetItem(int id) {
-                return JsonConvert.DeserializeObject<Dictionary<string, GItem>>(WClient.DownloadString("http://www.gw2spidy.com/api/v0.9/json/item/" + id))["result"];
-			}
-			
-			public GItem GetItem(string name){
+                Items = JsonConvert.DeserializeObject<GAllItemsResponse>(WClient.DownloadString("http://www.gw2spidy.com/api/v0.9/json/all-items/all")).Result;
+
+            }
+
+            public GItem GetItem(int id)
+            {
+                return Items.Find((GItem g) => g.DataId == id);
+                //return JsonConvert.DeserializeObject<Dictionary<string, GItem>>(WClient.DownloadString("http://www.gw2spidy.com/api/v0.9/json/item/" + id))["result"];
+            }
+
+            public GItem GetItem(string name)
+            {
                 return Items.Find((GItem g) => g.Name == name);
-			}
-		}
-	}
+            }
+        }
+    }
 }
